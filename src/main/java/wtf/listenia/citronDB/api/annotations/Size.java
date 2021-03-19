1b3142da7b3306a262a9ad1309f413ee6c9e8f7c
360matt <mattbacq2004@gmail.com>
2021-03-20 00:37:53 +0100

    Annotation for special key with size settings

---
+++ src/main/java/wtf/listenia/citronDB/api/annotations/Size.java
@@ -0,0 +1,9 @@
+package wtf.listenia.citronDB.api.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Size {
+    int size () default 1024;
+}
