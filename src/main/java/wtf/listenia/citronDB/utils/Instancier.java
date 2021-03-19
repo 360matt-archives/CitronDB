5b5fd56add88d9e297129d1bae34f915b048bc11
360matt <mattbacq2004@gmail.com>
2021-03-20 00:36:51 +0100

    Fusion

--- src/main/java/wtf/listenia/citronDB/utils/Instancier.java
+++ src/main/java/wtf/listenia/citronDB/utils/Instancier.java
@@ -1,7 +1,5 @@
 package wtf.listenia.citronDB.utils;
 
-import wtf.listenia.citronDB.internal.MainCitron;
-
 public class Instancier {
     /**
      * Allows to recover the default data of the chosen structure
@@ -10,15 +8,15 @@ public class Instancier {
      * @return the default data of the structure (instance)
      */
     public static <D> D getEmptyRaw (final Class<D> structure) {
-        if (!MainCitron.types.containsKey(structure)) {
+        if (!ExpirableCache.types.containsKey(structure)) {
             try {
                 final D res = structure.newInstance();
-                MainCitron.types.put(structure, res);
+                ExpirableCache.types.put(structure, res);
                 return res;
             } catch (InstantiationException | IllegalAccessException e) {
                 return null;
             }
         }
-        return (D) MainCitron.types.get(structure);
+        return (D) ExpirableCache.types.get(structure);
     }
 }
