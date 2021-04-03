42c73159804c8de1253b61c9332b2deb30bffabd
360matt <mattbacq2004@gmail.com>
2021-04-03 23:24:34 +0200

    Fixed bugs

--- src/main/java/wtf/listenia/citronDB/api/TableManager.java
+++ src/main/java/wtf/listenia/citronDB/api/TableManager.java
@@ -22,15 +22,15 @@ public class TableManager <D> {
         this.database = database;
         this.name = name;
         this.defaultInstance = struct;
-        this.defaultAsMap = new HashMap<>() {{
-            try {
-                for (final Field field : defaultInstance.getFields()) {
-                    put(field.getName(), field.get(defaultInstance));
-                }
-            } catch (final IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }};
+        this.defaultAsMap = new HashMap<>();
+
+        try {
+            final D def = struct.newInstance();
+            for (final Field field : defaultInstance.getFields())
+                this.defaultAsMap.put(field.getName(), field.get(def));
+        } catch (final IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 
     public final void createTable () {
@@ -38,29 +38,26 @@ public class TableManager <D> {
     }
 
     public final void createTable (final boolean update) {
-        try {
-            final StringJoiner sj = new StringJoiner(", ");
-            boolean hasPrimary = false;
+        final StringJoiner sj = new StringJoiner(", ");
+        boolean hasPrimary = false;
 
-            final Field[] fields = this.defaultInstance.getFields();
-            for (final Field field : fields) {
-
-                final Unique unique = field.getAnnotation(Unique.class);
-                if (unique != null) {
-                    sj.add(ColumnType.getFormat(field, false));
-                } else if (!hasPrimary) {
-                    final Primary primary = field.getAnnotation(Primary.class);
-                    if (primary != null) {
-                        hasPrimary = true;
-                        sj.add(ColumnType.getFormat(field, false));
-                    }
-                } else {
+        final Field[] fields = this.defaultInstance.getFields();
+        for (final Field field : fields) {
+            final Unique unique = field.getAnnotation(Unique.class);
+            if (unique != null) {
+                sj.add(ColumnType.getFormat(field, false));
+            } else if (!hasPrimary) {
+                final Primary primary = field.getAnnotation(Primary.class);
+                if (primary != null) {
+                    hasPrimary = true;
                     sj.add(ColumnType.getFormat(field, false));
                 }
+            } else {
+                sj.add(ColumnType.getFormat(field, false));
             }
+        }
 
-            final Statement stmt = this.database.getStatementWithException();
-
+        try (final Statement stmt = this.database.getStatementWithException()) {
             final String sql = "CREATE TABLE IF NOT EXISTS `" + this.name + "` (" + sj.toString() + ");";
             final int status = stmt.executeUpdate(sql);
 
@@ -78,10 +75,8 @@ public class TableManager <D> {
 
 
     public final void deleteTable () {
-        try {
-            final Statement stmt = this.database.getStatementWithException();
+        try (final Statement stmt = this.database.getStatementWithException();) {
             stmt.execute("DROP TABLE IF EXISTS `" + this.name + "`");
-            stmt.close();
         } catch (final SQLException e) {
             e.printStackTrace();
         }
@@ -89,12 +84,15 @@ public class TableManager <D> {
 
 
     public void updateStructure () {
-        try {
-            final Map<String, Field> local = new HashMap<>();
-            final List<String> distant = new ArrayList<>();
+        final Map<String, Field> local = new HashMap<>();
+        final List<String> distant = new ArrayList<>();
 
-            final Statement stmt = this.database.getStatementWithException();
-            final ResultSet rs = stmt.executeQuery("SELECT * FROM `" + this.name + "` WHERE 1 = 0;"); // select nothing
+        final String sql = "SELECT * FROM `" + this.name + "` WHERE 1 = 0;";
+
+        try (
+                final Statement stmt = this.database.getStatementWithException();
+                final ResultSet rs = stmt.executeQuery(sql);
+         ) {
             final ResultSetMetaData meta = rs.getMetaData();
             for (int i = 0; i < meta.getColumnCount(); i++)
                 distant.add(meta.getColumnName(i + 1));
@@ -107,8 +105,6 @@ public class TableManager <D> {
                 stmt.close();
                 return;
             }
-
-
 
 
             final List<String> toCreate = new ArrayList<>(local.keySet());
@@ -125,17 +121,17 @@ public class TableManager <D> {
                 action.add("ADD(" + toCreate.stream()
                         .map(x -> ColumnType.getFormat(local.get(x), false))
                         .collect(Collectors.joining(",")) +
-                ")");
+                        ")");
                 mustSend = true;
-            }
+                }
             if (toDelete.size() > 0) {
                 action.add("DROP " + String.join(",DROP ", toDelete));
                 mustSend = true;
             }
             if (toModify.size() > 0) {
                 action.add("MODIFY " + toModify.stream()
-                            .map(x -> ColumnType.getFormat(local.get(x), false))
-                            .collect(Collectors.joining(",MODIFY "))
+                        .map(x -> ColumnType.getFormat(local.get(x), false))
+                        .collect(Collectors.joining(",MODIFY "))
                 );
                 mustSend = true;
             }
@@ -144,8 +140,6 @@ public class TableManager <D> {
                 // deepcode ignore Sqli: < Tkt bro >
                 stmt.execute("ALTER TABLE `" + this.name + "` " + action.toString() + ";");
 
-
-            stmt.close();
         } catch (final SQLException e) {
             e.printStackTrace();
         }
@@ -153,14 +147,13 @@ public class TableManager <D> {
 
 
     public final void insert (final D line) {
-        try {
-            final Field[] fields = defaultInstance.getFields();
+        final Field[] fields = defaultInstance.getFields();
+        if (fields.length >= 1) {
+            final StringJoiner columnsList = new StringJoiner(",");
+            final List<Object> valuesList = new ArrayList<>();
+            final StringJoiner interro = new StringJoiner(",");
 
-            if (fields.length >= 1) {
-                final StringJoiner columnsList = new StringJoiner(",");
-                final List<Object> valuesList = new ArrayList<>();
-                final StringJoiner interro = new StringJoiner(",");
-
+            try {
                 for (final Field field : fields) {
                     columnsList.add(field.getName());
                     valuesList.add(field.get(line));
@@ -168,50 +161,44 @@ public class TableManager <D> {
                 }
 
                 final String columns = " (" + columnsList.toString() + ")";
-                final PreparedStatement stmt = this.database.getConnection().prepareStatement(
-                        "INSERT INTO `" + this.name + "` " + columns + " VALUES (" + interro.toString() + ")"
-                );
-                for (int i = 0; i < valuesList.size(); i++)
-                    stmt.setObject(i + 1, valuesList.get(i));
+                final String sql = "INSERT INTO `" + this.name + "` " + columns + " VALUES (" + interro.toString() + ")";
 
-                stmt.executeUpdate();
-                stmt.close();
+                try (final PreparedStatement stmt = this.database.getConnection().prepareStatement(sql);) {
+                    for (int i = 0; i < valuesList.size(); i++)
+                        stmt.setObject(i + 1, valuesList.get(i));
+
+                    stmt.executeUpdate();
+                } catch (final SQLException e) {
+                    e.printStackTrace();
+                }
+            } catch (final IllegalAccessException e) {
+                e.printStackTrace();
             }
-        } catch (final SQLException | IllegalAccessException e) {
-            e.printStackTrace();
         }
     }
 
     public final void insert (final RowBuilder builder) {
-        try {
-            if (builder.datas.size() >= 1) {
-                for (final Map.Entry<String, Object> entry : defaultAsMap.entrySet())
-                    builder.datas.putIfAbsent(entry.getKey(), entry.getValue());
+        if (builder.datas.size() >= 1) {
+            for (final Map.Entry<String, Object> entry : defaultAsMap.entrySet())
+                builder.datas.putIfAbsent(entry.getKey(), entry.getValue());
 
-                StringJoiner columns = new StringJoiner(",");
-                StringJoiner preformat = new StringJoiner(",");
+            final StringJoiner columns = new StringJoiner(",");
+            final StringJoiner preformat = new StringJoiner(",");
 
-                for (final String key : builder.datas.keySet()) {
-                    columns.add(key);
-                    preformat.add("?");
-                }
+            for (final String key : builder.datas.keySet()) {
+                columns.add(key);
+                preformat.add("?");
+            }
 
-                final PreparedStatement stmt = this.database.getConnection().prepareStatement(
-                        "INSERT INTO `" + this.name + "` (" + columns + ") VALUES (" + preformat.toString() + ")"
-                );
-
-                columns = null; // GC
-                preformat = null; // GC
-
+            final String sql = "INSERT INTO `" + this.name + "` (" + columns + ") VALUES (" + preformat.toString() + ")";
+            try (final PreparedStatement stmt = this.database.getConnection().prepareStatement(sql);) {
                 int i = 1;
                 for (final Object value : builder.datas.values())
                     stmt.setObject(i++, value);
-
                 stmt.executeUpdate();
-                stmt.close();
+            } catch (final SQLException e) {
+                e.printStackTrace();
             }
-        } catch (final SQLException e) {
-            e.printStackTrace();
         }
     }
 
@@ -243,13 +230,12 @@ public class TableManager <D> {
 
 
     public final boolean exist (final RowBuilder pattern) {
-        try {
-            final ResultSet rs = getResultForOneLine(pattern);
+        try (
+                final PreparedStatement statement = getStatementForOneLine(pattern);
+                final ResultSet rs = statement.executeQuery();
+        ) {
             final boolean state = rs.next();
-
-            rs.close();
             rs.getStatement().close();
-
             return state;
         } catch (final SQLException e) {
             e.printStackTrace();
@@ -264,25 +250,26 @@ public class TableManager <D> {
 
     public final D getLine (final RowBuilder pattern) {
         D content = null;
-        try {
-            if (pattern.datas.size() > 0) {
-                final ResultSet rs = getResultForOneLine(pattern);
-                final ResultSetMetaData data = rs.getMetaData();
 
+        if (pattern.datas.size() > 0) {
+            try (
+                    final PreparedStatement statement = getStatementForOneLine(pattern);
+                    final ResultSet rs = statement.executeQuery();
+            ) {
+                final ResultSetMetaData data = rs.getMetaData();
                 if (rs.next()) {
                     content = this.defaultInstance.newInstance();
                     for (int c = 1; c <= data.getColumnCount(); c++) {
                         final Object obj = rs.getObject(c);
-                        if (obj != null)
+                        if (obj != null) {
+
                             this.defaultInstance.getField(data.getColumnName(c)).set(content, obj);
+                        }
                     }
                 }
-
-                rs.close();
-                rs.getStatement().close();
+            } catch (final SQLException | NoSuchFieldException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
             }
-        } catch (final SQLException | NoSuchFieldException | IllegalAccessException | InstantiationException e) {
-            e.printStackTrace();
         }
         return content;
     }
@@ -290,65 +277,59 @@ public class TableManager <D> {
 
     public final Set<D> getLines (final RowBuilder pattern, int limit) {
         final Set<D> res = new HashSet<>();
-        try {
-            if (pattern.datas.size() > 0) {
-                final StringBuilder interro = new StringBuilder();
+        if (pattern.datas.size() > 0) {
+            final StringBuilder interro = new StringBuilder();
 
-                final Iterator<String> iter = pattern.datas.keySet().iterator();
-                if (iter.hasNext())
-                    interro.append(iter.next()).append("=?");
-                while (iter.hasNext())
-                    interro.append(",AND ").append(iter.next()).append("=?");
+            final Iterator<String> iter = pattern.datas.keySet().iterator();
+            if (iter.hasNext())
+                interro.append(iter.next()).append("=?");
+            while (iter.hasNext())
+                interro.append(",AND ").append(iter.next()).append("=?");
 
-                final PreparedStatement stmt = this.database.getConnection().prepareStatement(
-                        "SELECT " + limit + " FROM `" + this.name + "` WHERE " + interro.toString()
-                );
-
+            final String sql = "SELECT " + limit + " FROM `" + this.name + "` WHERE " + interro.toString();
+            try (final PreparedStatement stmt = this.database.getConnection().prepareStatement(sql)) {
                 int i = 1;
                 for (final Object object : pattern.datas.values())
                     stmt.setObject(i++, object);
 
-                final ResultSet rs = stmt.executeQuery();
-                final ResultSetMetaData data = rs.getMetaData();
 
-                while (limit-- > 0 && rs.next()) {
-                    final D content = this.defaultInstance.newInstance();
+                try (final ResultSet rs = stmt.executeQuery()) {
+                    final ResultSetMetaData data = rs.getMetaData();
 
-                    for (int c = 1; c <= data.getColumnCount(); c++) {
-                        final Object obj = rs.getObject(c);
-                        if (obj != null)
-                            this.defaultInstance.getField(data.getColumnName(c)).set(content, obj);
+                    while (limit-- > 0 && rs.next()) {
+                        final D content = this.defaultInstance.newInstance();
+
+                        for (int c = 1; c <= data.getColumnCount(); c++) {
+                            final Object obj = rs.getObject(c);
+                            if (obj != null)
+                                this.defaultInstance.getField(data.getColumnName(c)).set(content, obj);
+                        }
+                        res.add(content);
                     }
-                    res.add(content);
                 }
-                rs.close();
-                stmt.close();
+            } catch (final SQLException | NoSuchFieldException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
             }
-        } catch (final SQLException | NoSuchFieldException | IllegalAccessException | InstantiationException e) {
-            e.printStackTrace();
         }
         return res;
     }
 
 
     public final void update (final RowBuilder pattern, final RowBuilder replacement) {
-        try {
-            if (replacement.datas.size() > 0) {
-                for (final Map.Entry<String, Object> entry : defaultAsMap.entrySet())
-                    replacement.datas.putIfAbsent(entry.getKey(), entry.getValue());
+        if (replacement.datas.size() > 0) {
+            for (final Map.Entry<String, Object> entry : defaultAsMap.entrySet())
+                replacement.datas.putIfAbsent(entry.getKey(), entry.getValue());
 
-                final StringJoiner repComa = new StringJoiner(",");
-                final StringJoiner patComa = new StringJoiner(" AND ");
+            final StringJoiner repComa = new StringJoiner(",");
+            final StringJoiner patComa = new StringJoiner(" AND ");
 
-                for (final String key : replacement.datas.keySet())
-                    repComa.add(key + "=?");
-                for (final String key : pattern.datas.keySet())
-                    patComa.add(key + "=?");
+            for (final String key : replacement.datas.keySet())
+                repComa.add(key + "=?");
+            for (final String key : pattern.datas.keySet())
+                patComa.add(key + "=?");
 
-                final PreparedStatement stmt = this.database.getConnection().prepareStatement(
-                        "UPDATE `" + this.name + "` SET " + repComa.toString() + " WHERE " + patComa.toString()
-                );
-
+            final String sql = "UPDATE `" + this.name + "` SET " + repComa.toString() + " WHERE " + patComa.toString();
+            try (final PreparedStatement stmt = this.database.getConnection().prepareStatement(sql)) {
                 int i = 1;
                 for (final Object val : replacement.datas.values())
                     stmt.setObject(i++, val);
@@ -356,10 +337,9 @@ public class TableManager <D> {
                     stmt.setObject(i++, val);
 
                 stmt.executeUpdate();
-                stmt.close();
+            } catch (final SQLException e) {
+                e.printStackTrace();
             }
-        } catch (final SQLException e) {
-            e.printStackTrace();
         }
     }
 
@@ -368,24 +348,22 @@ public class TableManager <D> {
 
     // ______________________________________________________________________________________ //
 
-    private ResultSet getResultForOneLine (final RowBuilder pattern) throws SQLException {
+    private PreparedStatement getStatementForOneLine (final RowBuilder pattern) throws SQLException {
         final StringBuilder interro = new StringBuilder();
 
         final Iterator<String> iter = pattern.datas.keySet().iterator();
         if (iter.hasNext())
             interro.append(iter.next()).append("=?");
         while (iter.hasNext())
-            interro.append(",AND ").append(iter.next()).append("=?");
+            interro.append(" AND ").append(iter.next()).append("=?");
 
-        final PreparedStatement stmt = this.database.getConnection().prepareStatement(
-                "SELECT 1 FROM `" + this.name + "` WHERE " + interro.toString()
-        );
+        final String sql = "SELECT * FROM `" + this.name + "` WHERE " + interro.toString();
 
+        final PreparedStatement stmt = this.database.getConnection().prepareStatement(sql);
         int i = 1;
         for (final Object object : pattern.datas.values())
             stmt.setObject(i++, object);
-
-        return stmt.executeQuery();
+        return stmt;
     }
 
 }
