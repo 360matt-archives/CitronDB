e557d01172fe6e388c172835e42103b1bdbdb62f
360matt <mattbacq2004@gmail.com>
2021-07-08 19:18:25 +0200

    New test with my local Wamp-db. 1s saved for 100K lines

--- src/main/test/SomeTests.java
+++ src/main/test/SomeTests.java
@@ -1,55 +1,61 @@
 import fr.i360matt.citronDB.api.Database;
 import fr.i360matt.citronDB.api.annotations.Size;
 import fr.i360matt.citronDB.api.annotations.Unique;
-import fr.i360matt.citronDB.api.builders.RowBuilder;
 import fr.i360matt.citronDB.api.TableManager;
-import fr.i360matt.citronDB.utils.DebugTime;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class SomeTests {
 
     public static class ExampleStucture {
-        @Unique(size = 50)
+        @Unique()
+        @Size
         public String test = "truc";
         public String truc = "jaaj";
-        @Size(size = 20)
-        public int caillou = 0;
     }
 
-    public static void main (final String[] args) throws NoSuchFieldException {
+    public static void main (final String[] args) {
 
         Database db2 = new Database() {{
-            setHost("freedb.tech");
+            setHost("127.0.0.1");
             setPort(3306);
-            setDbName("freedbtech_souris");
-            setUsername("freedbtech_hey");
-            setPassword("cmabite");
+            setDbName("social");
+            setUsername("root");
+            setPassword("");
             connect();
         }};
 
-        TableManager<ExampleStucture> tableManager = db2.getTable("wsh", ExampleStucture.class);
 
-        DebugTime debugTime = new DebugTime();
-        debugTime.start();
+
+
+        /*
+        ExampleStucture ah = new ExampleStucture();
+        ah.caillou = 56;
+
+        tableManager.insert(ah);
+
+         */
+
+
+
+        TableManager<ExampleStucture> tableManager = db2.getTable("uneTable", ExampleStucture.class);
 
         tableManager.createTable(true);
         // will create the table, or if existe, update these.
 
+        DebugTime debugTime = new DebugTime();
+        debugTime.start();
+
+        for (int i = 0; i < 100_000; i++) {
+            ExampleStucture inst = new ExampleStucture();
+            inst.test = "Woaaw";
+
+            tableManager.insert(inst);
+        }
+
         debugTime.printElapsed();
 
-        RowBuilder line = new RowBuilder() {{
-            define("test", 100);
-            define("truc", "20~~é-|--éé~~##");
-        }};
-
-        RowBuilder replacement = new RowBuilder() {{
-            define("test", "caillou");
-        }};
 
 
-        tableManager.update(line, replacement);
+
 
 
 
